7549d6964ebe69a18ac6b13ca018bda4926cd836
Alessandro Paganelli <alessandro.paganelli@gmail.com>
2025-12-06 21:58:51 +0100

    WIP: adds character tests (still to complete)

--- src/app/Character.cpp
+++ src/app/Character.cpp
@@ -2,8 +2,10 @@
 
 void Character::Move(PlayerInput::Move move) {
     // Implementation of movement logic
+    // TODO
 }
 
 void Character::Act(PlayerInput::Action action) {
     // Implementation of action logic
+    // TODO
 }
--- src/app/GameEngine.cpp
+++ src/app/GameEngine.cpp
@@ -7,5 +7,33 @@ void GameEngine::Tick(TickInfo info) {
               << ", move: " << info.Move << ", action: " << info.Action
               << std::endl;
 
-    _canvas.PresentCharacter(info.PlayerIndex, {0, 0});
+    // _canvas.BeginDrawing();
+    // _canvas.RefreshBackground(position);
+    //
+    // const auto CanvasRefresher{}; // raii object to call begindrawing +
+    // enddrawing
+    //
+    // character = GetCharacter(info.PlayerIndex);
+    // if (!character)
+    //  return;
+    //
+    // if (info.Move != None) {
+    //     const auto currentPosition = character->GetPosition();
+    //     const auto speed = character->GetSpeed();
+    //
+    //     // responsible to check if the movement is valid inside the map,
+    //     // given invalid positions etc
+    //     const auto movementContext = MakeMovementContext(currentPosition,
+    //     move, speed);
+    //
+    //     character->Move(movementContext); // this calls an onMoved callback
+    //     on the engine
+    // }
+    //
+    // if (info.Interaction != None) {
+    //     character->Interact(interactionContext); // this calls an
+    //     onInteracted callback on the engine
+    // }
+    //
+    // _canvas.EndDrawing();
 }
